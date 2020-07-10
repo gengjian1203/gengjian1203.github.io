db7719ad78dc3bd065530ed60311b6c718e97a7f
gengjian1203 <gengjian1203@foxmail.com>
2020-07-10 22:10:13 +0800

    博客配置，引用gulp库，CI开启编译后压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,54 @@
-// 引入需要的模块
 var gulp = require('gulp');
+
+//Plugins模块获取
 var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
-var imagemin = require('gulp-imagemin');
+var minifyhtml = require('gulp-minify-html');
+var imagemin = require('gulp-imagemin')
 
-// 压缩public目录下所有html文件, minify-html是任务名, 设置为default，启动gulp压缩的时候可以省去任务名
-gulp.task('minify-html', function() {
-    return gulp.src('./public/**/*.html') // 压缩文件所在的目录
-        .pipe(htmlclean())
-        .pipe(htmlmin({
-            removeComments: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true,
-        }))
-        .pipe(gulp.dest('./public')) // 输出的目录
-});
-
-// 压缩css
-gulp.task('minify-css', function() {
-    return gulp.src(['./public/**/*.css','!./public/js/**/*min.css'])
-        .pipe(minifycss({
-            compatibility: 'ie8'
-        }))
+// 压缩 public 目录 css文件
+gulp.task('minify-css', function () {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
         .pipe(gulp.dest('./public'));
 });
-// 压缩js
-gulp.task('minify-js', function() {
-    return gulp.src(['./public/**/.js','!./public/js/**/*min.js'])
+
+// 压缩 public 目录 html文件
+gulp.task('minify-html', function () {
+    return gulp.src('./public/**/*.html')
+        .pipe(minifyhtml())
+        .pipe(gulp.dest('./public'))
+});
+
+// 压缩 public/js 目录 js文件
+gulp.task('minify-js', function () {
+    return gulp.src([
+      './public/**/*.js', 
+      '!./public/js/**.js'])
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
-// 压缩图片
-gulp.task('minify-images', function() {
-    return gulp.src(['./public/**/*.png','./public/**/*.jpg','./public/**/*.gif'])
+
+// 压缩public/posts 目录 图片文件
+gulp.task('minify-img', function () {
+    return gulp.src('./public/**/*.*')
         .pipe(imagemin(
-        [imagemin.gifsicle({'optimizationLevel': 3}), 
-        imagemin.mozjpeg({'progressive': true}), 
-        imagemin.optipng({'optimizationLevel': 5}), 
-        imagemin.svgo()],
-        {'verbose': true}))
+            [
+                imagemin.gifsicle({
+                    'optimizationLevel': 3
+                }),
+                imagemin.jpegtran({
+                    'progressive': true
+                }),
+                imagemin.optipng({
+                    'optimizationLevel': 7
+                }),
+                imagemin.svgo()
+            ], {
+                'verbose': true
+            }))
         .pipe(gulp.dest('./public'))
 });
-// gulp 4.0 适用的方式
-gulp.task('default', gulp.parallel('minify-html','minify-css','minify-js','minify-images'
-));
+
+// 分别执行css、heml、js和图片的压缩任务
+gulp.task('build', gulp.series('minify-css', 'minify-html', 'minify-js', 'minify-img'));
