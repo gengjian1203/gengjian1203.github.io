7e17b20a9ef55974704b7615b21da451921e1245
gengjian1203 <gengjian1203@foxmail.com>
2022-05-29 23:20:29 +0800

    feat(page)更新文章内容

--- script/showUrl.js
+++ script/showUrl.js
@@ -1,4 +1,5 @@
-console.log("https://gengjian1203.github.io/");
+const timestamp = new Date().getTime();
+console.log(`https://gengjian1203.github.io/?t=${timestamp}`);
 console.log(
-  "https://prod-5gkxku5cdb510bb2-1259256375.tcloudbaseapp.com/gengjian1203/index.html"
+  `https://prod-5gkxku5cdb510bb2-1259256375.tcloudbaseapp.com/gengjian1203/index.html?t=${timestamp}`
 );
