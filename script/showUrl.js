1df58a1f1972a8dd136e9a3b75a2ec6b08b5c0fb
Hugh Geng <gengjian1203@foxmail.com>
2024-01-27 17:25:33 +0800

    config(config): update config

--- script/showUrl.js
+++ script/showUrl.js
@@ -1,3 +1,3 @@
 const timestamp = new Date().getTime();
 console.log(`https://gengjian1203.github.io/?t=${timestamp}`);
-console.log(`https://www.orz2.top/gengjian1203/#/?t=${timestamp}`);
+console.log(`https://www.orz2.online/gengjian1203/#/?t=${timestamp}`);
